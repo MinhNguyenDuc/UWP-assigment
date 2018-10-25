d194e8a1c9def6d8939440896d86fe472c5201f9
Minh Nguyen Duc <nguyenducminh24101998@gmail.com>
2018-10-25 19:59:17 +0700

    new push

---
+++ ExamUWP/Views/Page1.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ExamUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page1 : Page
+    {
+        public Page1()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void SaveFileEvent(object sender, RoutedEventArgs e)
+        {
+            string fileName = FileName.Text;
+            string fileContent = FileContent.Text;
+
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, fileContent);
+            Debug.WriteLine("OK");
+        }
+    }
+}
---
+++ ExamUWP/Views/Page2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ExamUWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            this.InitializeComponent();
+            GetListFileAsync();
+        }
+
+        public async void GetListFileAsync()
+        {
+            var Files = await ApplicationData.Current.LocalFolder.GetFilesAsync();            
+            foreach (var file in Files)
+            {
+                ListFile.Items.Add(file.Name);                
+            }
+        }
+
+        private async void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Debug.WriteLine(ListFile.SelectedItem);
+            var Files = await ApplicationData.Current.LocalFolder.GetFilesAsync();
+            foreach (var file in Files)
+            {
+                if(file.Name == ListFile.SelectedItem.ToString())
+                {
+                    FileContent.Text = await Windows.Storage.FileIO.ReadTextAsync(file);
+                }
+            }
+        }
+    }
+}
